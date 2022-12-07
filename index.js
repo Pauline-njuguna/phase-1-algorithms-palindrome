1e25a7d2855fbbaa9aab58aa78529a641bb131a1
pauline-njunguna <paulinenjunguna.w@gmail.com>
2022-12-07 13:33:21 +0300

    Done

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  if (word.length <= 1) {
+    return true;
+  } else {
+    if (word[0] === word[word.length - 1]) {
+      isPalindrome(word.slice(1, -1));
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
